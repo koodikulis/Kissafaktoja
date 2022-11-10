ad072cf910165bf2816c0a327b17854f677256fe
Kalle <ketola.kalle@gmail.com>
2022-11-10 14:12:00 +0200

    Copy to clipboard -functionality added.

--- Kissafaktoja/MainPage.xaml.cs
+++ Kissafaktoja/MainPage.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
 using System.Threading.Tasks;
+using TextCopy;
 
 namespace Kissafaktoja;
 
 public partial class MainPage : ContentPage
 {
-	
+
 	public MainPage()
 	{
 		InitializeComponent();
@@ -14,18 +15,22 @@ public partial class MainPage : ContentPage
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		string responseBody = await GetCatFact();
-        Cat kissa = JsonConvert.DeserializeObject<Cat>(responseBody);
-        CatLabel.Text = kissa.fact;
-        SemanticScreenReader.Announce(Catbtn.Text);
+		Cat kissa = JsonConvert.DeserializeObject<Cat>(responseBody);
+		CatLabel.Text = kissa.fact;
+		SemanticScreenReader.Announce(Catbtn.Text);
 	}
 
 	private async Task<string> GetCatFact()
 	{
-        HttpClient client = new HttpClient();
-        HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
-        response.EnsureSuccessStatusCode();
-        string responseBody = await response.Content.ReadAsStringAsync();
-        return responseBody;
+		HttpClient client = new HttpClient();
+		HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
+		response.EnsureSuccessStatusCode();
+		string responseBody = await response.Content.ReadAsStringAsync();
+		return responseBody;
 	}
-}
 
+	private async void OnCopyClicked(object sender, EventArgs e)
+	{
+        await ClipboardService.SetTextAsync(CatLabel.Text);
+    }
+}
