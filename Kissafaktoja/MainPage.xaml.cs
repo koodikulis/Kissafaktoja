7ab2177466125f5c2cb9606541f5c84f24e6dffb
Kalle <ketola.kalle@gmail.com>
2022-11-10 16:42:35 +0200

    WinOS windows size is now set to 500 X 900.

--- Kissafaktoja/MainPage.xaml.cs
+++ Kissafaktoja/MainPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Newtonsoft.Json;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using TextCopy;
 
 namespace Kissafaktoja;
@@ -8,43 +7,43 @@ namespace Kissafaktoja;
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
-	{
-		InitializeComponent();
+    public MainPage()
+    {
+        InitializeComponent();
         if (DeviceInfo.Current.Platform != DevicePlatform.WinUI)
         {
-			Copybtn.IsVisible= false;
+            Copybtn.IsVisible = false;
         }
     }
 
-	private async void OnCounterClicked(object sender, EventArgs e)
-	{
-		string responseBody = await GetCatFact();
-		Cat kissa = JsonConvert.DeserializeObject<Cat>(responseBody);
-		CatLabel.Text = kissa.fact;
-		SemanticScreenReader.Announce(Catbtn.Text);
-	}
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        string responseBody = await GetCatFact();
+        Cat kissa = JsonConvert.DeserializeObject<Cat>(responseBody);
+        CatLabel.Text = kissa.fact;
+        SemanticScreenReader.Announce(Catbtn.Text);
+    }
 
-	private async Task<string> GetCatFact()
-	{
-		HttpClient client = new HttpClient();
-		HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
-		response.EnsureSuccessStatusCode();
-		string responseBody = await response.Content.ReadAsStringAsync();
-		return responseBody;
-	}
+    private async Task<string> GetCatFact()
+    {
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+        return responseBody;
+    }
 
-	private async void OnCopyClicked(object sender, EventArgs e)
-	{
-	
-			try
-			{
-				await ClipboardService.SetTextAsync(CatLabel.Text);
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine(ex, "MOBILE NOT SUPPORTED.");
-			}
- 
+    private async void OnCopyClicked(object sender, EventArgs e)
+    {
+
+        try
+        {
+            await ClipboardService.SetTextAsync(CatLabel.Text);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex, "MOBILE NOT SUPPORTED.");
+        }
+
     }
 }
